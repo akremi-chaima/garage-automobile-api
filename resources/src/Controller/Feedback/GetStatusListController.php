f467bdbe8a82599f7ac19da552ed187a79cc1d7d
Chaima <chaima.akremi.1997@gmail.com>
2024-01-22 19:59:13 +0100

    [EVOL] - Rename API url

--- resources/src/Controller/Feedback/GetStatusListController.php
+++ resources/src/Controller/Feedback/GetStatusListController.php
@@ -32,7 +32,7 @@ class GetStatusListController extends AbstractController
     /**
      * Get feedback status list
      *
-     * @Route("/api/private/status/list", methods={"GET"})
+     * @Route("/api/private/feedbacks/status/list", methods={"GET"})
      *
      * @OA\Tag(name="Feedback")
      *
