e97939e214e6d012cf537fa7dfbaa0785156ccd6
Chaima <chaima.akremi.1997@gmail.com>
2024-02-05 21:07:35 +0100

    [EVOl] - Add migration to create opening hours table

---
+++ resources/migrations/Version20240205200336.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240205200336 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('
+            CREATE TABLE `opening_hours` (
+                `id` INT NOT NULL AUTO_INCREMENT,
+                `day` VARCHAR(10) NOT NULL,
+                `morning_start_hour` VARCHAR(5) NULL,
+                `morning_end_hour` VARCHAR(5) NULL,
+                `afternoon_start_hour` VARCHAR(5) NULL,
+                `afternoon_end_hour` VARCHAR(5) NULL,
+                PRIMARY KEY (`id`)
+            )
+        ');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE `opening_hours`');
+    }
+}
